8ca580944ec5214c5eb794e7426a675bf3e1d4b3
seokzoo <seokzoo@seokzooui-MacBookAir.local>
2025-11-06 13:29:52 +0900

    hello world

---
+++ common.c
@@ -0,0 +1,74 @@
+#include "common.h"
+
+void putchar(char ch);
+
+void *memset(char *begin, char c, unsigned int n)
+{
+    for (unsigned int i=0;i<n;i++) {
+        *(begin + i) = c;
+    }
+    return begin;
+}
+
+void printf(const char *fmt, ...)
+{
+    va_list vargs;
+    va_start(vargs, fmt);
+
+    while (*fmt) {
+        if (*fmt == '%') {
+            fmt++;
+            switch (*fmt) {
+                case '\0':
+                    goto end;
+
+                case '%':
+                    putchar('%');
+                    break;
+
+                case 's': {
+                    const char *s = va_arg(vargs, const char *);
+                    while (*s) {
+                        putchar(*s);
+                        s++;
+                    }
+                    break;
+                }
+
+                case 'd': {
+                    unsigned value = (unsigned)va_arg(vargs, int);
+                    if ((signed)value < 0) {
+                        putchar('-');
+                        value = -value;
+                    }
+                    unsigned divisor = 1;
+                    
+                    while (value / divisor > 9)
+                        divisor *= 10;
+
+                    while (divisor) {
+                        putchar('0' + value / divisor);
+                        value %= divisor;
+                        divisor /= 10;
+                    }
+                    break;
+                }
+
+                case 'x': {
+                    unsigned value = va_arg(vargs, unsigned);
+                    for (int i = 7; i >= 0; i--) {
+                        unsigned nibble = (value >> (i * 4)) & 0xf;
+                        putchar("0123456789abcdef"[nibble]);
+                    }
+                    break;
+                }
+            }
+        } else {
+            putchar(*fmt);
+        }
+        fmt++;
+    }
+
+end:
+    va_end(vargs);
+}
--- kernel.c
+++ kernel.c
@@ -1,27 +1,49 @@
+#include "common.h"
+#include "kernel.h"
 
-extern char *__bss, *__bss_end, *__stack_top;
+extern char __bss[], __bss_end[], __stack_top[];
 
-void memset(char *begin, char c, unsigned int n)
+struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4, long arg5, long fid, long eid)
 {
-    for (unsigned int i=0;i<n;i++) {
-        *(begin + i) = c;
-    }
+    register long a0 __asm__("a0") = arg0;
+    register long a1 __asm__("a1") = arg1;
+    register long a2 __asm__("a2") = arg2;
+    register long a3 __asm__("a3") = arg3;
+    register long a4 __asm__("a4") = arg4;
+    register long a5 __asm__("a5") = arg5;
+    register long a6 __asm__("a6") = fid;
+    register long a7 __asm__("a7") = eid;
+
+    __asm__ __volatile__("ecall"
+        : "=r"(a0), "=r"(a1)
+        : "r"(a0), "r"(a1), "r"(a2), "r"(a3), "r"(a4), "r"(a5), "r"(a6), "r"(a7)
+        : "memory");
+    return (struct sbiret) {.error = a0, .value = a1};
+}
+
+void putchar(char ch)
+{
+    sbi_call(ch, 0, 0, 0, 0, 0, 0, 1); 
 }
 
 void main(void) 
 {
     memset((char *)__bss, 0, (__bss_end - __bss));
 
-    while(1);
+    printf("%d + %d\n = %d\n", 3, 4, 3+4);
+    
+    while(1) {
+        __asm__ __volatile("wfi");
+    }
 }
 
 __attribute__((section(".text.boot")))
 __attribute__((naked))
 void boot(void) {
     __asm__ __volatile__(
-        "mv sp, %0\n"
+        "mv sp, %[stack_top]\n"
         "j main\n"
         :
-        : "r"(__stack_top)
+        : [stack_top] "r"(__stack_top)
     );
 }
